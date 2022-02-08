90aa9149afe8d764d4404afd2f5bf331985bd782
Austin Comstock <98965925+austinbcomstock@users.noreply.github.com>
2022-02-08 13:45:28 -0600

    Update kucoin-to-cointracker.py

--- kucoin-to-cointracker.py
+++ kucoin-to-cointracker.py
@@ -5,17 +5,14 @@ def load_kucoin_file(file):
     return pd.read_csv(file, index_col=False)
 
 def convert_kucoin_to_cointracker(df):
-
-    # # Strip the clock times to only leave the dates
-    # df.entryDate = pd.to_datetime(df['tradeCreatedAt']).apply(lambda x: x.date())
     
-    # Need to split symbol into receive and sent currencies columns
+    # Need to split symbol into 'Received Currency' and 'Sent Currency' columns
     df[['Received Currency', 'Sent Currency']] = df['symbol'].str.split('-', expand=True)
     
     # Filter on only the columns we care about from Kucoin
     df = df[['tradeCreatedAt', 'size', 'Received Currency', 'funds', 'Sent Currency', 'fee', 'feeCurrency']]
 
-    # Change the column names to what Cointracker wants
+    # Change the column names to what Cointracker expects
     df.columns = ['Date', 'Received Quantity', 'Received Currency', 'Sent Quantity', 'Sent Currency', 'Fee Amount', 'Fee Currency']
 
     return df
@@ -28,14 +25,20 @@ def main(args):
     # Ignoring deprecated feature warning
     pd.set_option('mode.chained_assignment', None)
 
+    # Load the CSV file
     df = load_kucoin_file(args.file)
+    
+    # Convert the transactions
     cointracker_dataframe = convert_kucoin_to_cointracker(df)
-    export_csv(cointracker_dataframe, filename='./kucoin.csv')
+    
+    # Export the Cointracker CSV file
+    export_csv(cointracker_dataframe, filename=args.output)
         
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=f'')
     # Positional
-    parser.add_argument('-f', '--file', type=str, metavar='',help="the file location of the Kucoin export csv file")
+    parser.add_argument('-f', '--file', type=str, metavar='',help="Location of the Kucoin Margin export CSV file")
+    parser.add_argument('-o', '--output', type=str, default='./kucoin.csv', metavar='',help="Location to output the Cointracker compatible CSV file")
     args = parser.parse_args()
     main(args)
